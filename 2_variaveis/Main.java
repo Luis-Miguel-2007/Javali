ca805d642340f9f4b313dde9ed7e47a75418c625
Luis-Miguel-2007 <lmiguel.batistela@gmail.com>
2026-08-20 01:18:26 -0300

    revisão de fundamentos

---
+++ 2_variaveis/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+ public static void main(String[] args) {
+    String nome = "Savalo";
+    String sobrenome = "Horse";
+    int idade = 85;
+    double altura =1.80;
+    boolean empregado = true;
+    String telefone = " 15 997570038";
+    String endereco = " Pocoto Pocoto";
+ String cep = " .com.com.com";
+String cidade = " Pe de pano";
+String uf = " SP";
+double salario_bruto =  15000.00;
+double valor_hora_extra = 150.00;
+
+System.out.println("Nome do aoluno " + nome + " " + sobrenome);
+System.out.println ("idade " + idade);
+System.out.println ("empregado " + empregado);
+System.out.println ("telefone " + telefone);
+System.out.println ("endereço " + endereco);
+System.out.println ("cep " + cep);
+System.out.println ("cidade " + cidade); 
+System.out.println ("uf " + uf);
+System.out.println ("salario bruto " + salario_bruto); 
+System.out.println ("valor hora extra " + valor_hora_extra);
+    
+}
+}

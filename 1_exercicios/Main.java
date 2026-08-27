18dd36087c743ca8433dcc19ec7c3262f7f4fa23
Luis-Miguel-2007 <lmiguel.batistela@gmail.com>
2026-08-27 01:24:31 -0300

    NOVOS TESTES

---
+++ 1_exercicios/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+    public static void main(String[] args){
+// boolean Lampada = false;
+// if (Lampada) {
+//     System.out.println("Apagar luz porque olhos doem");
+// } else {
+//     System.out.println("Acender a luz porque nao enxergo");
+// }
+// boolean Lampada = false;
+// String mensagem = (Lampada == true) ? "Apagar luz porque olhos doem" : "Acender a luz porque nao enxergo";
+// System.out.println(mensagem);
+
+ int Idade = 19;
+ if (Idade <=12) {
+     System.out.println("Criança");
+ } 
+ else if (Idade >= 13 && Idade <= 18){
+   System.out.println("Adolescente");
+}
+         else if (Idade >= 19 && Idade <= 59){
+        System.out.println("Adulto");
+    }
+
+    else{
+    System.out.println("Idoso");
+}
+
+
+
+    }
+}

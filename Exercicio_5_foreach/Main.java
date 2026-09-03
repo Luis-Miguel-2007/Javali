7f3843b3e6153a3dd603ed559ab4e5f5edf0a8c9
Luis-Miguel-2007 <lmiguel.batistela@gmail.com>
2026-09-03 01:23:21 -0300

    avancos

---
+++ Exercicio_5_foreach/Main.java
@@ -0,0 +1,39 @@
+public class Main {
+    public static void main(String[] args) {
+        // String[] alunos= {"Miranata", "Savalo", "Aeronauta"};
+        // alunos[0] = "Mariazinha";
+        // System.out.println("Qtde de Alunos " + alunos.length);
+
+        // for (String estudante : alunos ){
+        //     System.out.println(estudante);
+
+        // String[] produtos= {"PAO" ,"LEITE", "ACUCAR", "FARINHA", "OVOS"};
+        // for (String Itens : produtos) {
+        //     System.out.println(Itens);
+        // }
+
+        // String[] produtos = {"PAO", "LEITE", "ACUCAR", "FARINHA", "OVOS", "BATATA","PIRAO","MACA","CEBOLA", "REPOLHO", "OREGANO", "CHOCOLATE", "TORTA","MORANGOS","MANGA" };
+
+        // for (int i = 0; i < produtos.length; i++) {
+        //   System.out.println(produtos[i]);
+        // }
+
+
+    
+        int[] numeros = {1, 20, -3, 0, -2};
+        for (int item : numeros){
+            if (item <0)
+        System.out.println("O VALOR DE " + item +" NEGATIVO");
+            if (item ==0)
+        System.out.println("O VALOR DE " + item +" ZERO");
+            if (item >0)
+        System.out.println("O VALOR DE " + item +" POSITIVO");
+
+        }
+}
+
+
+
+
+    }
+
---
+++ Exercicios_3_While/Main.java
@@ -0,0 +1,21 @@
+//crie uma variavel i=100
+//repita o loop exibindo cada valor,
+//até que i seja igual a 0
+public class Main {
+    public static void main(String[] args){
+
+int i = 100 ;
+
+while (i>0){
+        System.out.println(i);
+        i--;
+
+    }
+    int a = 50;
+
+while (a<60) {
+    System.out.println(a);
+    a++;
+}
+}
+}
